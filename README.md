458f772275e7ccf39dc95a34d9ab631ec0c2c886
jfco09 <89312221+jfco09@users.noreply.github.com>
2023-10-22 21:31:20 +0200

    upload ej1 & update gitignore

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ejerciciosSACB
- 
+Ejercicios comentados: 
+- 1: Maquinas
+- 3: Electro
+- 5: PID
+Ejercicios que no comprendo 100%:
+- 2: Falta revisar
+- 4: No comprendo como obtiene los 20 puntos
--- ejercicio1/main.c
+++ ejercicio1/main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
-#include
-int main()
-{
-    printf("Hello World!\n");
+
+int main(){
+    //Declaración de variables
+    int ordenes[] = { 5, 3, 2, 6, 3, 4, 1, 6, 4, 8 }; // Número de órdenes ejecutadas por cada máquina
+    int libres[] = { 1, 1, 0, 0, 0, 1, 0, 0, 1, 1 }; // Buleanos que indican qué máquinas están libres
+    int nMaquinas = 10; // Número de máquinas existentes
+    //Variables
+    int i;
+    int primera = 1;
+    int posmin = 0;
+    int maqlibre = 0;
+
+    //bucle for
+    for (i = 0; i < nMaquinas; i++)
+    {
+        //Ver si la máquina i es libre
+        if (libres[i])
+        {
+            //encuntra la primera maquina libre
+            if (primera)
+            {
+                posmin = i;
+                primera = 0;
+            }
+
+            //busca minimo
+            if (ordenes[i] < ordenes[posmin])
+            {
+                posmin = i;
+            }
+
+            maqlibre++;
+
+        }
+
+    }
+
     return 0;
 }
 
